1a13af2cc62a470b6372e1c0d68a6641921325fa
Brendan Seabrook <brendanseabrooK@gmail.com>
2018-01-13 13:31:03 -0500

    Added tests

--- Jan2017AssignmentTests/Jan2017AssignmentTests.swift
+++ Jan2017AssignmentTests/Jan2017AssignmentTests.swift
@@ -11,26 +11,60 @@ import XCTest
 
 class Jan2017AssignmentTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testYelpAPI() {
+        YelpDataProvider.shared.processResponse(data: YelpTesting.getMockedResponse(forCondition: .Normal)!, completion: { (restaurants, error) in
+            XCTAssert(error == nil, "Should not produce error")
+            XCTAssert(restaurants != nil, "Restaurants should be present")
+            XCTAssert(restaurants?.count == 3, "There should be 3 restaurants present")
+        })
+            
+        YelpDataProvider.shared.processResponse(data: YelpTesting.getMockedResponse(forCondition: .BadAPIKey)!) { (restaurants, error) -> (Void) in
+            XCTAssert(error != nil, "Should have error")
+            XCTAssert(error == .BadAPIKey, "Should be BadAPIKey error not \(String(describing: error!.self))")
+        }
+        
+        YelpDataProvider.shared.processResponse(data:
+        YelpTesting.getMockedResponse(forCondition: .MalformedRequest)!) { (restaurants, error) -> (Void) in
+            
+            XCTAssert(error != nil, "Should have error")
+            XCTAssert(error == .BadRequest, "Should be BadRequest error not \(String(describing: error!.self))")
+        }
+        
+        YelpDataProvider.shared.processResponse(data:
+        YelpTesting.getMockedResponse(forCondition: .MalformedResponse)!) { (restaurants, error) -> (Void) in
+            
+            XCTAssert(error != nil, "Should have error")
+            XCTAssert(error == .BadResponse , "Should be BadResponse error not \(String(describing: error!.self))")
         }
     }
-    
+}
+
+enum RestaurantDataProviderOperatingCondition {
+    case Normal
+    case BadAPIKey
+    case MalformedRequest
+    case MalformedResponse
+}
+
+protocol RestaurantDataProviderTests {
+    static func getMockedResponse(forCondition:RestaurantDataProviderOperatingCondition) -> Data?
+}
+
+class YelpTesting : RestaurantDataProviderTests {
+    static func getMockedResponse(forCondition condition: RestaurantDataProviderOperatingCondition) -> Data? {
+        let bundle = Bundle(for: self)
+        let filePath:String?
+        switch condition {
+        case .Normal:
+            filePath = bundle.path(forResource: "NormalMock", ofType: "json")
+        case .BadAPIKey:
+            filePath = bundle.path(forResource: "BadAPIKeyMock", ofType: "json")
+        case .MalformedRequest:
+            filePath = bundle.path(forResource: "MalformedRequestMock", ofType: "json")
+        case .MalformedResponse:
+            filePath = bundle.path(forResource: "MalformedResponseMock", ofType: "json")
+        }
+        
+        return try? Data(contentsOf: URL(fileURLWithPath: filePath!))
+    }
 }
